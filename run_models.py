650d2cf9768c5468cdcaff0bea21b232759c1689
MEIHongY <hongyuan@MEIHongYs-Mac.local>
2016-11-05 13:59:16 -0400

    change: process all features first, to speed training up

--- run_models.py
+++ run_models.py
@@ -243,6 +243,7 @@ def train_selgen(input_train):
     print "finish training"
     #
 #
+#
 def train_selgen_eval_angeli(input_train):
     '''
     this function is called to train Sel Gen model
@@ -292,6 +293,8 @@ def train_selgen_eval_angeli(input_train):
             'size_batch': input_train['size_batch']
         }
     )
+    #
+    data_process.process_all_info(['train', 'dev'])
     #
     #TODO: build the model
     print "building model ... "
